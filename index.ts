012c03ebbaba02fea69e0759232649595a2008e4
bleeno <109549513+bleenobtw@users.noreply.github.com>
2025-12-14 21:16:58 +0000

    chore: test workflow

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { logger } from "hono/logger";
 const app = new Hono();
 app.use(logger());
 
-app.get("/", (c) => c.json({ success: true }, 200));
+app.get("/", (c) => c.json({ success: true, health: "ok" }, 200));
 
 Bun.serve({
   fetch: app.fetch,
